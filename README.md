2f8742985f689f7ceaf950e8a09dfe2549a75e18
mihirk <mihir.khatwani@gmail.com>
2014-12-22 14:37:03 +0530

    Mihir | Adding proper license, documentation and revising the project.clj

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # clojure-test-datasetup
 
-A Clojure library designed to ... well, that part is up to you.
+Clojure Test Datasetup provides a thin layer for setting up data for integration tests.
 
 ## Usage
 
-FIXME
+setup-dataset and tear-down-dataset are two methods which take in the path of a sample data setup json
+and the db-spec to setup data on a test database and tear it down.
 
 ## License
 
 Copyright © 2014 FIXME
 
-Distributed under the Eclipse Public License either version 1.0 or (at
+Distributed under the Affero General Public License either version 3.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,11 +1,22 @@
-(defproject clojure-test-datasetup "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.5"]
-                 [org.clojure/java.jdbc "0.3.6"]
-                 [midje "1.6.3"]
-                 [org.xerial/sqlite-jdbc "3.8.7"]]
-  :plugins [[lein-midje "3.1.3"]])
+(defproject org.bahmni/clojure-test-datasetup "1.0-SNAPSHOT"
+            :description "Setup test data from JSON for Integration tests in Clojure. Can be used with all test frameworks."
+            :url "https://github.com/Bhamni/clojure_test_datasetup"
+            :license {:name "Affero General Public License,"
+                      :url  "http://www.gnu.org/licenses/agpl-3.0.html"}
+            :dependencies [[org.clojure/clojure "1.6.0"]
+                           [org.clojure/data.json "0.2.5"]
+                           [org.clojure/java.jdbc "0.3.6"]
+                           [midje "1.6.3"]
+                           [org.xerial/sqlite-jdbc "3.8.7"]]
+            :resource-paths ["resource"]
+            :group-id "org.ict4h"
+            :artifact-id "clojure-test-datasetup"
+            :packaging "jar"
+            :aliases {"test" ["midje"]}
+            :name "clojure-test-datasetup"
+            :scm {:name                 "git"
+                  :tag                  "HEAD"
+                  :url                  "https://github.com/Bhamni/clojure_test_datasetup.git"
+                  :connection           "scm:git:git@github.com:Bhamni/clojure_test_datasetup.git"
+                  :developer-connection "scm:git:git@github.com:Bhamni/clojure_test_datasetup.git"}
+            :plugins [[lein-midje "3.1.3"]])
